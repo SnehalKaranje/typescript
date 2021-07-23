8ac90434cf9f82fe4f9b92e01bd9df5ed72784d8
SnehalKaranje <snehalkaranje9@gmail.com>
2021-07-23 16:23:03 +0530

    Decorators - Class, Method, Property, Parameters

---
+++ Decorators/TodoListComponent.d.ts
@@ -0,0 +1,8 @@
+import { Todo } from "./Model";
+import '//code.jquery.com/jquery-1.12.1.min.js';
+export default class TodoListComponent {
+    private $el;
+    constructor(el: HTMLElement);
+    render(todos: Todo[]): void;
+    private renderTodo;
+}
